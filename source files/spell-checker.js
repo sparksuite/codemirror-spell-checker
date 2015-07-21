ac371b71433c1f07f830d1824adc76f99fc71e40
Wes Cossick <wes@nextstepwebs.com>
2015-07-20 23:17:58 -0500

    Load asynchronously for much better performance

--- source files/spell-checker.js
+++ source files/spell-checker.js
@@ -1,24 +1,50 @@
 CodeMirror.defineMode("spell-checker", function(config, parserConfig) {
-	function _readFile(path, charset) {
-		if(!charset) charset = "ISO8859-1";
+	// Initialize data
+	var num_loaded = 0;
+	var aff_data = "";
+	var dic_data = "";
+	var typo;
 	
-		var req = new XMLHttpRequest();
-		req.open("GET", path, false);
 	
-		if(req.overrideMimeType)
-			req.overrideMimeType("text/plain; charset=" + charset);
+	// Load AFF/DIC data
+	var xhr_aff = new XMLHttpRequest();
+	xhr_aff.open("GET", "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.aff", true);
+	xhr_aff.onload = function (e) {
+		if (xhr_aff.readyState === 4 && xhr_aff.status === 200) {
+			aff_data = xhr_aff.responseText;
+			num_loaded++;
+			
+			if(num_loaded == 2){
+				typo = new Typo("en_US", aff_data, dic_data, {
+					platform: 'any'
+				});
+			}
+		}
+	};
+	xhr_aff.send(null);
 	
-		req.send(null);
+	var xhr_dic = new XMLHttpRequest();
+	xhr_dic.open("GET", "https://cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.dic", true);
+	xhr_dic.onload = function (e) {
+		if (xhr_dic.readyState === 4 && xhr_dic.status === 200) {
+			dic_data = xhr_dic.responseText;
+			num_loaded++;
+			
+			if(num_loaded == 2){
+				typo = new Typo("en_US", aff_data, dic_data, {
+					platform: 'any'
+				});
+			}
+		}
+	};
+	xhr_dic.send(null);
 	
-		return req.responseText;
-	}
 	
-	var typo = new Typo("en_US", _readFile("//cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.aff"), _readFile("//cdn.jsdelivr.net/codemirror.spell-checker/latest/en_US.dic"), {
-		platform: 'any'
-	});
-
-	var rx_word = "!\"#$%&()*+,-./:;<=>?@[\\\\\\]^_`{|}~ ";
+	// Define what separates a word
+	var rx_word = "!\"#$%&()*+,-./:;<=>?@[\\]^_`{|}~ ";
 	
+	
+	// Create the overlay and such
 	var overlay = {
 		token: function(stream, state) {
 			var ch = stream.peek();
@@ -35,7 +61,7 @@ CodeMirror.defineMode("spell-checker", function(config, parserConfig) {
 			}
 
 			if(typo && !typo.check(word))
-				return "spell-error"; //CSS class: cm-spell-error
+				return "spell-error"; // CSS class: cm-spell-error
 
 			return null;
 		}
